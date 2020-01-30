4db2fa944dcdd51bb7f026565dd131d90977edfb
DepressingUtopian <DepressingUtopian@gmail.com>
2020-01-30 16:19:10 +0700

    Touching the Seekbar toggle switches now changes

--- app/src/main/java/com/project/videoeditor/VideoTimeline.java
+++ app/src/main/java/com/project/videoeditor/VideoTimeline.java
@@ -72,40 +72,13 @@ public class VideoTimeline extends Fragment {
             public void onRangeChanged(RangeSeekBar view, float leftValue, float rightValue, boolean isFromUser) {
                 if (isFromUser) {
 
-                    Log.d("onRangeChanged", String.valueOf((seekBar.getProgressWidth() / seekBar.getMaxProgress() * SBR.getProgress()) + seekBar.getPaddingLeft()));
-                    Log.d("onRangeChanged", String.valueOf((seekBar.getProgressWidth() / seekBar.getMaxProgress() * SBL.getProgress()) + seekBar.getPaddingLeft()));
-
-
-                   // Log.d("onRangeChanged", String.valueOf(seekBar.getScrollX()));
-                    videoEditBar.setmLeft((int) ((seekBar.getProgressWidth() / seekBar.getMaxProgress() * SBL.getProgress()) + seekBar.getPaddingLeft() + SBL.getThumbWidth() / 2));
-                    videoEditBar.setmRight((int) ((seekBar.getProgressWidth() / seekBar.getMaxProgress() * SBR.getProgress()) + seekBar.getPaddingLeft() + SBR.getThumbWidth() / 2));
-
-                    if(tempLeftValue != leftValue) {
-                        long millisSBL = (long) SBL.getProgress() % 1000;
-                        long secondSBL = ((long) SBL.getProgress() / 1000) % 60;
-                        long minuteSBL = ((long) SBL.getProgress() / (1000 * 60)) % 60;
-                        // long hourSBL = ((long)SBL.getProgress() / (1000 * 60 * 60)) % 24;
-                        tempLeftValue = leftValue;
-                        SBL.setIndicatorText(String.format("%02d:%02d.%d",minuteSBL, secondSBL, millisSBL));
-                        pVideoView.seekTo((int)SBL.getProgress());
-                    }
-                    if(tempRightValue != rightValue) {
-                        long millisSBR = (long) SBR.getProgress() % 1000;
-                        long secondSBR = ((long) SBR.getProgress() / 1000) % 60;
-                        long minuteSBR = ((long) SBR.getProgress() / (1000 * 60)) % 60;
-                        // long hourSBR = ((long)SBR.getProgress() / (1000 * 60 * 60)) % 24;
-                        tempRightValue = rightValue;
-                        SBR.setIndicatorText(String.format("%02d:%02d.%d",minuteSBR, secondSBR, millisSBR));
-                        pVideoView.seekTo((int)SBR.getProgress());
-                    }
-                    videoEditBar.invalidate();
-                    //seekBar.getPr;
+                    updateTimeline(leftValue,rightValue);
                 }
             }
 
             @Override
             public void onStartTrackingTouch(RangeSeekBar view, boolean isLeft) {
-
+                updateTimeline(SBL.getProgress(),SBR.getProgress());
             }
 
             @Override
@@ -140,6 +113,31 @@ public class VideoTimeline extends Fragment {
         BitmapFactory.Options bmOptions = new BitmapFactory.Options();
         Bitmap bitmap = BitmapFactory.decodeFile(image.getAbsolutePath(),bmOptions);
         videoFramesCollage.setImageBitmap(bitmap);
+    }
+    private void updateTimeline(float leftValue, float rightValue)
+    {
+        videoEditBar.setmLeft((int) ((seekBar.getProgressWidth() / seekBar.getMaxProgress() * SBL.getProgress()) + seekBar.getPaddingLeft() + SBL.getThumbWidth() / 2));
+        videoEditBar.setmRight((int) ((seekBar.getProgressWidth() / seekBar.getMaxProgress() * SBR.getProgress()) + seekBar.getPaddingLeft() + SBR.getThumbWidth() / 2));
+
+        if(tempLeftValue != leftValue) {
+            long millisSBL = (long) SBL.getProgress() % 1000;
+            long secondSBL = ((long) SBL.getProgress() / 1000) % 60;
+            long minuteSBL = ((long) SBL.getProgress() / (1000 * 60)) % 60;
+            // long hourSBL = ((long)SBL.getProgress() / (1000 * 60 * 60)) % 24;
+            tempLeftValue = leftValue;
+            SBL.setIndicatorText(String.format("%02d:%02d.%d",minuteSBL, secondSBL, millisSBL));
+            pVideoView.seekTo((int)SBL.getProgress());
+        }
+        if(tempRightValue != rightValue) {
+            long millisSBR = (long) SBR.getProgress() % 1000;
+            long secondSBR = ((long) SBR.getProgress() / 1000) % 60;
+            long minuteSBR = ((long) SBR.getProgress() / (1000 * 60)) % 60;
+            // long hourSBR = ((long)SBR.getProgress() / (1000 * 60 * 60)) % 24;
+            tempRightValue = rightValue;
+            SBR.setIndicatorText(String.format("%02d:%02d.%d",minuteSBR, secondSBR, millisSBR));
+            pVideoView.seekTo((int)SBR.getProgress());
+        }
+        videoEditBar.invalidate();
     }
 
 }
